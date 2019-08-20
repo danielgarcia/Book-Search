ba5f7bef1b4468a553c63fce20c388d9db53a9d4
Daniel Garcia Alanis <daniel@dagasoft.com>
2019-08-19 21:03:11 -0700

    reconfigured webpack. added sass, babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import "./main.scss"
+
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = 'Hello webpack!';
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+  const element = document.createElement('div');
+
+  element.innerHTML = 'Hello webpack!';
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        "sass-loader"
+      ]
+    }]
   }
 };
